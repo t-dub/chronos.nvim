f287864b5e7736741692a0d19b97b0545ed6cdac
t-dub (Tom Wolf) <tom@t-dub.net>
2024-04-30 16:21:43 -0500

    Adjusts formatting to fix breakage

--- plugin/chronos.lua
+++ plugin/chronos.lua
@@ -1,34 +1,35 @@
 print("Chronos loaded in just in time")
 
 local date_formats = {
-	"%Y-%m-%d",
-	"%Y-%m-%d @ %H:%M",
+  "%Y-%m-%d",
+  "%Y-%m-%d @ %H:%M",
 }
 
 local current_format_index = 1
 local last_inserted_text = ""
 
 function cycle_date_format()
-	local bufnr = vim.api.nvim_get_current_buf()
-	local cursor = vim.api.nvim_win_get_cursor(0)
-	local format = date_formats[current_format_index]
-	local new_text = vim.fn.strftime(format)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local format = date_formats[current_format_index]
+  local new_text = vim.fn.strftime(format)
 
-	if last_inserted_text ~= "" then
-		local start_col = cursor[2] - #last_inserted_text
-		vim.api.nvim_buf_set_text(bufnr, cursor[1] - 1, start_col, cursor[1] - 1, cursor[2], { new_text })
-	else
-		vim.api.nvim_put({ new_text }, "c", false, true)
-	end
+  if last_inserted_text ~= "" then
+    local start_col = cursor[2] - #last_inserted_text
+    vim.api.nvim_buf_set_text(bufnr, cursor[1] - 1, start_col, cursor[1] - 1, cursor[2], {new_text})
+  else
+    vim.api.nvim_put({new_text}, "c", false, true)
+  end
 
-	last_inserted_text = new_text
-	current_format_index = current_format_index % #date_formats + 1
+  last_inserted_text = new_text
+  current_format_index = current_format_index % #date_formats + 1
 end
 
 function reset_feature()
-	current_format_index = 1
-	last_inserted_text = ""
+  current_format_index = 1
+  last_inserted_text = ""
 end
 
 vim.keymap.set("i", "<C-t>", "<Cmd>lua cycle_date_format()<CR>", { noremap = true })
 vim.cmd("autocmd InsertCharPre * lua reset_feature()")
+
